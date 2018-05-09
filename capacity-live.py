9c21a8e373e0e6fb9de57209e6349c2be3b23079
pschild <philippe.schild@gmail.com>
2018-05-09 10:49:58 +0200

    applied logging code for live video

--- capacity-live.py
+++ capacity-live.py
@@ -3,27 +3,63 @@ import numpy as np
 import time
 from imutils.video.pivideostream import PiVideoStream
 
-startTime = time.time()
-lastFrameTime = startTime
-intervalInSec = 2.0
 frameCounter = 0
+countedSeconds = 0
+logInterval = 1 #seconds
 
-cap = PiVideoStream().start()
+videoFps = 25
+cap = PiVideoStream(framerate=videoFps).start()
 time.sleep(2.0)
-#videoFps = cap.get(cv2.CAP_PROP_FPS)
 
 base = np.zeros((240,320) + (3,), dtype='uint8')
-#base = np.zeros((480,640) + (3,), dtype='uint8')
-#base = np.zeros((638,1280) + (3,), dtype='uint8')
-#AREA_PTS = np.array([[268,354], [519,436], [403,632], [15,632]])
 AREA_PTS = np.array([[0,0], [100,0], [100,100]])
 area_mask = cv2.fillPoly(base, [AREA_PTS], (255, 255, 255))[:, :, 0]
 all = np.count_nonzero(area_mask)
 
-def logToFile(second, capacity):
+# temporary variables
+global min
+global max
+global avg
+global cnt
+global sum
+
+min = 1.0
+max = 0.0
+avg = 0.0
+cnt = 0
+sum = 0
+
+def updateStats(capacity):
+	global cnt
+	global sum
+	global min
+	global max
+	global avg
+
+	cnt += 1
+	sum += capacity
+
+	if (capacity < min):
+		min = capacity
+
+	if (capacity > max):
+		max = capacity
+
+	avg = sum / cnt
+
+def resetAverage():
+	global cnt
+	global sum
+	global avg
+
+	cnt = 0
+	sum = 0
+	avg = 0.0
+
+def logToFile(countedSeconds, min, max, avg):
 	f = file('./carCounter.log','a')
 	timestamp = time.strftime("%Y%m%d-%H%M%S")
-	output = timestamp + ';' + str(second) + ';' + str(capacity) + "\r\n"
+	output = timestamp + ';' + str(countedSeconds) + ';' + str(min) + ';' + str(max) + ';' + str(avg) + "\r\n"
 	f.write(output)
 	f.close()
 
@@ -37,35 +73,35 @@ cv2.setMouseCallback('img', printMouseCoords)
 while 1:
 	frame = cap.read()
 	frameCounter += 1
+	# log every second
+	if (frameCounter % (videoFps * logInterval) == 0):
+		countedSeconds += logInterval
+		logToFile(countedSeconds, min, max, avg)
+		print "logged@{}".format(time.time())
+		resetAverage()
 
-	if (time.time() - lastFrameTime >= intervalInSec):
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-		cl1 = clahe.apply(frame)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+	cl1 = clahe.apply(frame)
 
-		edges = cv2.Canny(frame,50,70)
-		edges = ~edges
+	edges = cv2.Canny(frame,50,70)
+	edges = ~edges
 
-		blur = cv2.bilateralFilter(cv2.blur(edges,(21,21), 100),9,200,200)
+	blur = cv2.bilateralFilter(cv2.blur(edges,(21,21), 100),9,200,200)
 
-		_, threshold = cv2.threshold(blur,220,255,cv2.THRESH_BINARY)
-		t = cv2.bitwise_and(threshold,threshold,mask = area_mask)
+	_, threshold = cv2.threshold(blur,230,255,cv2.THRESH_BINARY)
+	t = cv2.bitwise_and(threshold,threshold,mask = area_mask)
 
-		free = np.count_nonzero(t)
-		capacity = 1 - float(free) / all
+	free = np.count_nonzero(t)
+	capacity = 1 - float(free) / all
 
-		#print "cap: {0}".format(capacity)
-		print "{}".format(time.time())
-		#currentSecond = frameCounter / videoFps
-		#logToFile(currentSecond, capacity)
-
-		lastFrameTime = time.time()
-
-		#cv2.imshow('img',edges)
+	#print "cap: {0}".format(capacity)
+	updateStats(capacity)
+	#print "min={},max={},avg={},sum={},cnt={}".format(min,max,avg,sum,cnt)
 
 	#cv2.fillPoly(frame, [AREA_PTS], (255, 0, 0))
 
-	cv2.imshow('img',frame)
+	#cv2.imshow('img',frame)
 	k = cv2.waitKey(30) & 0xff
 	if k == 27:
 		break
